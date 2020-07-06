e7bdd7796d99acc12dd79097835635fb14d510fd
Giordano Fechio <giordano.fechio@booking.com>
2020-07-06 11:28:32 +0200

    Terraform: Defining NFS to share TF states

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,12 @@
 provider "kubernetes" {
 }
 
+terraform {
+  backend "local" {
+    path = "/mnt/SSD/_terraform_states/deploy_metrics.tfstate"
+  }
+}
+
 resource "kubernetes_namespace" "deploy-metrics" {
   metadata {
     name = "deploy-metrics"
